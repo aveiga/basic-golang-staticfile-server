274e3c1d81d938f55b1f7f14df0d055f752ba7ee
André Veiga <a.branco.veiga@gmail.com>
2022-03-23 00:20:36 +0000

    using nerdctl for the keycloak image build, instead of Docker

--- build-keycloak-image.sh
+++ build-keycloak-image.sh
@@ -7,4 +7,4 @@ git clone git@github.com:keycloak/keycloak-containers.git
 cd keycloak-containers/server
 git checkout $VERSION
 # docker build -t "jboss/keycloak:${VERSION}" .
-docker build -t "aveiga/keycloak:${VERSION}" .
+nerdctl build -t "aveiga/keycloak:${VERSION}" .
